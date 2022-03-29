61f08e986018f9b6abd9095fc45d9a3822b60408
JBEDU20 <JBEDU20@JBEDU-20>
2022-03-29 16:23:10 +0900

    m

--- src/Circle.java
+++ src/Circle.java
@@ -1,6 +1,8 @@
 
 public class Circle {
-	private int x, int y, double radius;
+	private int x;
+	private int y;
+	private double radius;
 
 	public Circle(int x, int y, double radius) {
 		super();

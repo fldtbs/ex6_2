af0747d5ee4e47a05a0dea6248755748faf01c2f
JBEDU20 <JBEDU20@JBEDU-20>
2022-03-29 16:21:44 +0900

    ,

--- src/CircleEx.java
+++ src/CircleEx.java
@@ -3,8 +3,8 @@ public class CircleEx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Circle a = new Circle(,,);
-		Circle b = new Circle(,,);
+		Circle a = new Circle(2,3,5);
+		Circle b = new Circle(2,3,30);
 		System.out.println("¿ø a : " + a);
 		System.out.println("¿ø b : " + b);
 		if(a.equals(b)) {

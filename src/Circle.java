15c9257b4b7ff1d0f05741deb658a1f4fe462783
JBEDU20 <JBEDU20@JBEDU-20>
2022-03-29 16:20:44 +0900

    b

---
+++ src/Circle.java
@@ -0,0 +1,12 @@
+
+public class Circle {
+	private int x, int y, double radius;
+
+	public Circle(int x, int y, double radius) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+	}
+	
+}
